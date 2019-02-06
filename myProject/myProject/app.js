e703d3e4cf37ccfcd7208974da9049eb71f3ef31
Josh Anderson <47358777+academicSSB@users.noreply.github.com>
2019-02-06 15:19:13 -0500

    Add files via upload

---
+++ myProject/myProject/app.js
@@ -0,0 +1,23 @@
+/* function clock() {
+  const fullDate = new Date();
+  var hrs = fullDate.getHours();
+  var mins = fullDate.getMinutes();
+  var secs = fullDate.getSeconds();
+
+  if (hrs < 10){
+    hrs = "0" + hrs;
+  }
+  if (mins < 10){
+    mins = "0" + mins;
+  }
+  if (secs < 10){
+    secs = "0" + secs;
+  }
+
+  document.getElementById('hour').innerHTML = hrs;
+  document.getElementById('minute').innerHTML = ":" + mins;
+  document.getElementById('second').innerHTML = ":" + secs;
+}
+
+setInterval(clock, 100);
+*/
